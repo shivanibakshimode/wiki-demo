f9f706b113fdea2bab486ec918a521c54dff1c02
Shivani Bakshi <shivani.bakshi@mode.com>
2024-04-18 12:40:34 +0530

    Update vitest

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+describe("Sample test", () => {
+
+  describe("applyOperator", () => {
+    it("should work as expected", () => {
+      expect(true).toBe(true);
+      expect(false).toBe(false);
+    });
+  });
+});
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+export default {
+  // config options
+  clearScreen: false,
+  esbuild: {
+    legalComments: "none",
+  },
+  build: {
+    target: "esnext",
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["src/**/*.spec.ts"],
+    threads: false,
+    coverage: {
+      provider: "istanbul",
+      enabled: true,
+      reporter: ["text", "json-summary", "json", "html"],
+      reportOnFailure: true,
+    },
+  },
+};
